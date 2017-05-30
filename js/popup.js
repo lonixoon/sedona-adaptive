8dcad4712914a2853c893cf95f98c2d0f04d3474
lonixoon <soinov.alexey@yandex.ru>
2017-05-30 12:06:59 +0700

    мелкие исправления

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 var menuToggle = document.querySelector(".main-nav__toggle");
 var menuClosed = document.querySelector(".main-nav");
 
-var loginFormOpened = document.querySelector(".main-nav__login");
-var loginForm = document.querySelector(".login-form");
-var loginFormClosed = document.querySelector(".login-form__closed");
+// var loginFormOpened = document.querySelector(".main-nav__login");
+// var loginForm = document.querySelector(".login-form");
+// var loginFormClosed = document.querySelector(".login-form__closed");
 
-var formSubmit = document.querySelector(".form__submit");
-var formSubmitted = document.querySelector(".form__submitted");
-var formSubmittedClose = document.querySelector(".form__submitted-close");
+// var formSubmit = document.querySelector(".form__submit");
+// var formSubmitted = document.querySelector(".form__submitted");
+// var formSubmittedClose = document.querySelector(".form__submitted-close");
 
 
 
@@ -18,24 +18,24 @@ menuToggle.addEventListener("click", function(event) {
 })
 
 
-loginFormOpened.addEventListener("click", function(event) {
-  event.preventDefault();
-  loginForm.classList.add("login-form--opened");  
-  menuClosed.classList.remove("main-nav--opened");
-  menuClosed.classList.add("main-nav--closed");
-})
-loginFormClosed.addEventListener("click", function(event) {
-  event.preventDefault();
-  loginForm.classList.toggle("login-form--opened");
-})
+// loginFormOpened.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loginForm.classList.add("login-form--opened");  
+//   menuClosed.classList.remove("main-nav--opened");
+//   menuClosed.classList.add("main-nav--closed");
+// })
+// loginFormClosed.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loginForm.classList.toggle("login-form--opened");
+// })
 
 
-formSubmit.addEventListener("click", function(event) {
-  event.preventDefault();
-  formSubmitted.classList.add("form__submitted--open");
-})
+// formSubmit.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   formSubmitted.classList.add("form__submitted--open");
+// })
 
-formSubmittedClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  formSubmitted.classList.remove("form__submitted--open");
-})
+// formSubmittedClose.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   formSubmitted.classList.remove("form__submitted--open");
+// })
